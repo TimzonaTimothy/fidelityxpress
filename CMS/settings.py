103e2efaa9be639672f99b4b08ec3a3ca4ec4a06
TimzonaTimothy <timzonatimothy@gmail.com>
2024-08-12 13:57:46 -0700

    database

--- CMS/settings.py
+++ CMS/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'CMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'sterli51_fidelityxpress',
-#         'USER': 'sterli51_fidelityxpress_admin',
-#         'PASSWORD': 'fidelity88880000',
-#         'HOST': '176.74.18.130',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sterli51_fidelityxpress',
+        'USER': 'sterli51_fidelityxpress_admin',
+        'PASSWORD': 'fidelity88880000',
+        'HOST': '176.74.18.130',
+        'PORT': '5432',
+    }
+}
 
 
 
