7a5c8499727c107321d278dc8cfbcc5c39366e58
TimzonaTimothy <timzonatimothy@gmail.com>
2024-08-12 00:35:27 -0700

    first

--- CMS/settings.py
+++ CMS/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '$5a%%cj78_@&x045mb1bqe1el%$&5bnxd%qi=w*=xqb*txl2u-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '127.0.0.1','http://fidelityxpress.org/','fidelityxpress.org/']
+ALLOWED_HOSTS = ['*', '127.0.0.1','http://fidelityxpress.org/','fidelityxpress.org/','https://fidelityxpress.onrender.com']
 
 
 # Application definition
